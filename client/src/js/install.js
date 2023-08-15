8a89f0f0b318dc2d9a626368b49cc9fd76a1d2d9
Jonathan Goncalves <jonjigoncalves@gmail.com>
2023-08-15 01:12:40 -0400

    service worker issue

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,26 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let deferredPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    window.deferredPrompt = event;
+    deferredPrompt = event;
     butInstall.style.display = 'block';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const eventP = window.deferredPrompt;
-    if (!eventP) {
-        eventP.prompt();
-        const result = await  eventP.userChoice;
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const result = await  deferredPrompt.userChoice;
         if (result.outcome === 'accepted'){
             console.log('User Approved');
         }else {
             console.log('User Denied');
         }
         // hide the button after the user makes a choice 
-        window.deferredPrompt = null;
+        deferredPrompt = null;
         butInstall.style.display = 'none';
        }
 });
